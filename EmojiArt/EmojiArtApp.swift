953bcd2f84ddacae01b6ceb634369b7c345ee747
soylent <1593860+soylent@users.noreply.github.com>
2023-06-13 17:32:26 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by user on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
