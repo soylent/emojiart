2a449bbee914eae6e7e01904418abe9497fdab6f
soylent <1593860+soylent@users.noreply.github.com>
2023-06-13 18:01:47 -0600

    Add model

---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by user on 6/13/23.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+
+        var url: URL? {
+            switch self {
+            case .url(let url):
+                return url
+            default:
+                return nil
+            }
+        }
+
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data):
+                return data
+            default:
+                return nil
+            }
+        }
+    }
+}
